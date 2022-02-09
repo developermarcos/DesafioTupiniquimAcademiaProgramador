757254c494912f049ee55ecb7a898e42ce8ed0bc
Marcos Lima <developermarcos@outlook.com>
2022-02-09 16:15:36 -0300

    Add project files.

---
+++ DesafioTupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesafioTupiniquim.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
